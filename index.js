aa796b6fdf6318a4fe73cc3a6156d59d6eff842a
elaygl <elaygl@gmail.com>
2018-03-26 17:41:12 +0300

    Added basic button in the files page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const {Codeowner} = require('codeowners-api');
+// const {Codeowner} = require('codeowners-api');
 
-const getFiles = () => {
+const getChangedFiles = () => {
     const fileHeaders = document.querySelectorAll('div.file-header.js-file-header > div.file-info > a');
     const paths = [];
     fileHeaders.forEach(x => paths.push(x.text));
@@ -10,7 +10,7 @@ const getFiles = () => {
 
 const getUser = () => document.querySelector('meta[name="user-login"]').content;
 
-const start = async () => {
+const getRelevantFiles = async () => {
     const pathParts = window.location.pathname.split('/');
 
     const codeowner = new Codeowner(
@@ -24,10 +24,42 @@ const start = async () => {
         },
     );
 
-    const files = getFiles();
+    const files = getChangedFiles();
     const user = getUser();
 
     console.log(await codeowner.filterForCodeOwner(files, user));
 };
 
-start();
+const toggleElementDisplay = element => {
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+};
+
+let isFilterOn = false;
+const toggleFilerFiles = () => {
+    isFilterOn = !isFilterOn;
+
+    const files = document.querySelectorAll('#files > div > div');
+    files.forEach(x => toggleElementDisplay(x));
+};
+
+const getCodeownersButton = () => {
+    const button = document.createElement('button');
+    button.className = 'diffbar-item btn btn-sm btn-secondary codeowners-btn';
+    button.innerHTML = 'Show my files';
+    button.onclick = toggleFilerFiles;
+
+    return button;
+};
+
+const injectButton = () => {
+    const container = document.querySelector(
+        '#files_bucket > div.pr-toolbar.js-sticky.js-sticky-offset-scroll > div > div.float-right.pr-review-tools',
+    );
+    container.insertBefore(getCodeownersButton(), container.firstChild);
+};
+
+injectButton();
