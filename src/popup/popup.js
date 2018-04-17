ae801f0093df661155aca28dc4e103bd85264208
Noam Okman <noamokman@gmail.com>
2018-04-17 16:24:42 +0300

    show if the token is saved or not

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,8 +1,18 @@
-const handleSuccess = () => window.close()
+const githubTokenInput = document.getElementById('github_token');
 
-  const saveToken = () => {
-    var oauth_token = document.getElementById('github_token').value;
-    chrome.storage.sync.set({oauth_token}, handleSuccess);
+const handleSuccess = () => window.close();
+
+const saveToken = () => {
+  var oauth_token = githubTokenInput.value;
+  chrome.storage.sync.set({ oauth_token }, handleSuccess);
+}
+
+document.getElementById('save_token').addEventListener('click', saveToken);
+
+chrome.storage.sync.get('oauth_token', ({ oauth_token }) => {
+  if (!oauth_token) {
+    return;
   }
 
-  document.getElementById('save_token').addEventListener('click', saveToken);
+  githubTokenInput.value = oauth_token;
+});
