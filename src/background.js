b425dd00985f5603463f9ffb02df8fb6af97e83f
elaygl <elaygl@gmail.com>
2018-04-25 22:35:32 +0300

    fix button not appear on split/unified view change

--- src/background.js
+++ src/background.js
@@ -1,22 +1,14 @@
-
-let lastTriggered;
-
-chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     if (isFilesSection(details.url)) {
-        if (alreadyTriggered(details.url)) return;
-        lastTriggered = details.url
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            notifyContentScript(tabs, details.url)
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            notifyContentScript(tabs, details.url);
         });
-
     } else {
-        lastTriggered = null
+        lastTriggered = null;
     }
 });
 
-// Due to bug in chrome that fires twice on same event
-const alreadyTriggered = (url) => url === lastTriggered
+const notifyContentScript = (tabs, triggerUrl) =>
+    tabs[0] && chrome.tabs.sendMessage(tabs[0].id, {codeowners: 'background', location: triggerUrl});
 
-const notifyContentScript = (tabs, triggerUrl) => tabs[0] && chrome.tabs.sendMessage(tabs[0].id, { codeowners: 'background', location: triggerUrl });
-
-const isFilesSection = (url) => url && url.indexOf('github.com') > 0 && url.replace(/\?.*/i, '').endsWith('/files')
+const isFilesSection = url => url && url.indexOf('github.com') > 0 && url.replace(/\?.*/i, '').endsWith('/files');
